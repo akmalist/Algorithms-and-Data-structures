7c1696ce257487bdfae9e5c587caa35303900013
Aki <akmalist@gmail.com>
2019-11-17 08:47:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -51,5 +51,12 @@ function productOfArray(arr) {
 	
 	
 /////////////////////////////////
-	
+
+function recursiveRange(num){
+	if(num===0){
+	return 0;
+	}
+	return num + recursiveRange(num-1)
+}
+recursiveRange(10) // 55 
 	
