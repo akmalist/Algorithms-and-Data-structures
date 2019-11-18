01e560b90a91893baf9f0aa5c210cce0af67401c
Aki <akmalist@gmail.com>
2019-11-18 12:53:39 -0800

    Update app.js

--- app.js
+++ app.js
@@ -60,3 +60,15 @@ function recursiveRange(num){
 }
 recursiveRange(10) // 55 
 	
+/////////////////////////////////
+	
+	
+function reverse(str) {
+    if(str.length <=1){
+        return str;
+    }
+    return reverse(str.slice(1)) + str[0];
+    
+    
+    
+}
