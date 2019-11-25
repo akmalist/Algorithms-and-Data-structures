8a66d5de568df66bbb4b485c0d2f23617da0c4f7
Aki <akmalist@gmail.com>
2019-11-24 17:02:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -137,3 +137,48 @@ function findMatch(str,val){
 
 
 findMatch("wordakidakidadakiadd","aki")
+	
+	
+	
+/////////////////Buble sort//////
+	
+	//naive version
+	
+function bubbleSort(arr){
+  
+  for(let i = arr.length; i>0; i--){
+    //i-1 means that we want to shrink the i everytime when swapping j's
+       for(let j =0; j< i-1; j ++){
+        if(arr[j]> arr[j+1]){
+          let temp = arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+        }
+  }
+  }
+
+  return arr;
+
+
+  
+};
+	
+	//////////////O(n) version/////////
+
+function bubbleSort2(arr){
+  const swap = (arr, first, second)=>{
+    [arr[first],arr[second]]= [arr[second],arr[first]];
+  };
+
+  for(let i=arr.length; i > 0; i--){
+    for(let j=0; j<i-1; j++){
+      if(arr[j]>arr[j+1]){
+        swap(arr, j, j+1)
+      }
+    }
+  }
+
+  return arr;
+}
+
+bubbleSort2([15,2,99,12,55,25])
