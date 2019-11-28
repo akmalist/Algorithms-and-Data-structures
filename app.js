448ada2ecc5033adf43317f29f0c0c71d5ff4034
Aki <akmalist@gmail.com>
2019-11-28 13:19:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -230,3 +230,48 @@ function selectSort2(arr){
 }
 selectSort2([4,24,16,2,12,5,22,6,45])
 
+/////////////////////////////////Merging Sort///////////////////
+	
+
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+
+}
+
+function mergeSort(arr){
+  if(arr.length<=1){
+    return arr;
+  }
+  let middle = arr.length / 2;
+  let leftSide = mergeSort(arr.slice(0,middle));
+  let rightSide = mergeSort(arr.slice(middle))
+  return merge(leftSide, rightSide);
+ 
+}
+
+
+
+mergeSort([12, 15, 6, 2, 57, 5])
+
