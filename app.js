52eacf53e803af8e4a3bd4e37e612c773c902ab8
Aki <akmalist@gmail.com>
2020-02-18 15:55:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -275,3 +275,36 @@ function mergeSort(arr){
 
 mergeSort([12, 15, 6, 2, 57, 5])
 
+////////////////////Radix Sort///////////////////
+	
+	function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+const digitCount = num => {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+const mostDigits = nums => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+
+const radixSort = nums => {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBackets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      const digit = getDigit(nums[i], k);
+      digitBackets[digit].push(nums[i])
+    }
+    nums = [].concat(...digitBackets);
+  }
+  return nums;
+}
+radixSort([12, 22313, 12, 442, 221, 56])
