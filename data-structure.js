9910b69d039add1ad9a94dae6127259c762889df
Aki <akmalist@gmail.com>
2020-02-19 16:56:38 -0800

    Create data-structure.js

---
+++ data-structure.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+// const first = new Node("hey");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you?");
+
+class SinglyLinkedList{
+  constructor(){
+      this.head = null;
+      this.tail = null;
+      this.length= 0;
+    }
+    push(val){
+      const newNode = new Node(val)
+      if(!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+       }else{
+         this.tail.next= newNode;
+         this.tail = newNode; 
+       }
+        this.length++
+        return this;
+     }
+
+}
+
+const list = new SinglyLinkedList();
+list.push("hello");
+list.push("how are you?")
+ 
+
+
+
